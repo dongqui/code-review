47ce2de900697f62d04b684f1cab631e83ed511c
dongqui <dongqui9@gmail.com>
2024-12-11 11:51:30 +0900

    chore: lint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -29,5 +29,9 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "camelcase": "off",
+    "require-jsdoc": "off",
+    "max-len": "off",
   },
 };
+
